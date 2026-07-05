e3c3f30b0f5dfc93fba90c73b4aaeffda46a079e
nieshaa <monishaa@myinvoice2u.com>
2026-07-06 02:32:38 +0800

    Task 4

--- Strands Interview/interview-optional/SingleInstance.java
+++ Strands Interview/interview-optional/SingleInstance.java
@@ -10,15 +10,38 @@ package com.strands.spf;
  */
 public class SingleInstance {
 
-  public SingleInstance() {
+  // volatile ensures visibility across threads - without it, one thread could
+  // see a half-constructed object due to instruction reordering
+  private static volatile SingleInstance instance;
+
+  private SingleInstance() {
     // TODO Auto-generated constructor stub
+  }
+
+  public static SingleInstance getInstance()
+  {
+    if (instance == null)
+    {
+
+      synchronized (SingleInstance.class)
+      {
+        if(instance == null)
+        {
+          instance = new SingleInstance();
+        }
+      }
+    }
+
+    return instance;
   }
 
   /**
    * @param args
    */
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    SingleInstance a = SingleInstance.getInstance();
+    SingleInstance b = SingleInstance.getInstance();
+    System.out.println(a == b);
 
   }
 
